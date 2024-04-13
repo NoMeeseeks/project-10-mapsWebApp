ea0fb0e79651b40e6a20949baac558ae154574ee
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-13 10:40:21 -0500

    creacion del side-menu y personalizacion

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+
+  public menuItems: MenuItem[] = [
+    { name: 'Full Screen', route: '/maps/fullscreen' },
+    { name: 'Zoom range', route: '/maps/zoom' },
+    { name: 'Markers', route: '/maps/markers' },
+    { name: 'Properties', route: '/maps/properties' },
+  ];
 
 }
