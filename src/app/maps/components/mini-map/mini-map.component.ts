e1de237e14140616eeac41e95f96083a8791a797
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-15 16:09:38 -0500

    fin seccion 21

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import mapboxgl, { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+  @Input()
+  lngLat?: [number, number]
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    if (!this.divMap) { throw 'El mapa no se encontro' };
+    if (!this.lngLat) { throw 'La latitud y longitud no pueden ser nulas' };
+
+    const map = new mapboxgl.Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      interactive: false
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
 }
